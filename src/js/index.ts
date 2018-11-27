fc6517c2bb11637ee7cae55d8c8617d0bfbea860
Anders Jagd Lauritsen <ande4845@edu.easj.dk>
2018-11-27 09:44:10 +0100

    making sure

---
+++ src/js/index.ts
@@ -0,0 +1,29 @@
+class Tamagotchi {
+
+    //Properties
+    public username: string
+    public tamagotchiName: string
+    public gender: string
+    public mood: number
+    public hunger: number
+    public thirst: number
+    public boredom: number
+
+    constructor(UserName: string, TamagotchiName: string, Gender: string) {
+        this.username = UserName
+        this.tamagotchiName = TamagotchiName
+        this.gender = Gender
+        this.mood = 100
+        this.hunger = 100
+        this.thirst = 100
+        this.boredom = 100
+    }
+
+    //functions
+
+}
+
+
+
+
+
