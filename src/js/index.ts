22d0ffc78106d14e932feab17474781de335e12a
Anders Jagd Lauritsen <ande4845@edu.easj.dk>
2018-12-11 11:45:54 +0100

    createfriendadded

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,76 +4,133 @@ import axios, {
 
 //URI
 //Husk at ændre denne til den oprigtige Webservice når den er klar.
-let webserviceUri: string = "https://resttamagotchiservice.azurewebsites.net/api/"
+//let webserviceUri: string = "https://localhost:44300/api/"
+let webserviceUri: string = "https://ifriendwebservice.azurewebsites.net/api/"
+
 
 //Interfaces
 //Husk ingen uppercases! Why? NOBODY KNOWS
 
 interface ITask{
-    id: number
-    tamagotchiID: number
+    taskId: number
+    friendId: number
     taskName: string
-    taskReward: number
+    taskDesc: string
 }
 
 interface IUser{
-    id: number
-    userName: string
-    password: string
+    userId: number
+    username: string
+    userPassword: string
 }
 
 interface IFriend{
-    id: number
-    userID: number
-    friendName: string
+    friendsId: number
+    friendsName: string
     gender: boolean
-    mood: number
+    thirst: number
     hunger: number
-    boredom: number
+    task: number
+    fun: number
+    dress: number
+    ownerId: number
 }
 
 //Variabler
-let userIDfromName: number;
 
 //Inputs
 let loginUsernameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("loginUserNameInput")
 let loginPasswordInput: HTMLInputElement = <HTMLInputElement>document.getElementById("loginPasswordInput")
 
-let taskUsernameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("taskUserNameInput")
-let taskFriendnameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("taskFriendNameInput")
-let taskNameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("taskName")
+let IFriendNameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("IFriendNameInput")
+let releaseFriendInput: HTMLInputElement = <HTMLInputElement>document.getElementById("releaseIfriendNameInput")
 
-//Outputs
-let allTasksOutput: HTMLDivElement = <HTMLDivElement>document.getElementById("taskListOutput");
+let taskTitelInput: HTMLInputElement = <HTMLInputElement>document.getElementById("taskTitelInput")
+let taskFriendnameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("taskFriendNameInput")
+let taskDescInput: HTMLInputElement = <HTMLInputElement>document.getElementById("taskDescInput")
+let loadTaskFriendnameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("loadTaskFriendNameInput")
+let deleteTaskIdInput: HTMLInputElement = <HTMLInputElement>document.getElementById("deleteTaskidInput")
+
 
 //Selecters(Dropdowns)
+let IFriendGenderSelector: HTMLSelectElement = <HTMLSelectElement>document.getElementById("IFriendGenderSelect")
 let taskRewards: HTMLSelectElement = <HTMLSelectElement>document.getElementById("taskRewards");
 
 //Buttons
+window.onload = function(){ 
+    
+//UserButtons
 let LoginButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("LoginButton");
-LoginButtonElement.onclick = CheckLogin;
+if(LoginButtonElement != null){
+    LoginButtonElement.addEventListener("click", CheckLogin);
+}
+
+//IFriend Buttons
+let CreateIFriendButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("createIFriendButton");
+if(CreateIFriendButtonElement != null){
+    CreateIFriendButtonElement.addEventListener("click", createIFriend);
+}
+
+let LoadIFriendButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("loadButton");
+if(LoadIFriendButtonElement != null){
+    LoadIFriendButtonElement.addEventListener("click", GetAllIFriends);
+}
+
+let ReleaseIFriendButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("releaseFriendButton");
+if(ReleaseIFriendButtonElement != null){
+   ReleaseIFriendButtonElement.addEventListener("click", deleteIfriend);
+}
+
+//Task Buttons
+let OpretTaskButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("createTaskButton");
+if(OpretTaskButtonElement != null){
+    OpretTaskButtonElement.addEventListener("click", CreateTasks)
+}
 
 let HentTaskListeButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("loadTaskButton");
-HentTaskListeButtonElement.onclick = GetAllTasks;
+if(HentTaskListeButtonElement != null){
+    HentTaskListeButtonElement.addEventListener("click", GetIFriendTasks)
+}
 
-let OpretTaskButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("createTaskButton");
-OpretTaskButtonElement.onclick = PostTask;
+let DeleteTaskButtonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("deleteTaskButton");
+if(DeleteTaskButtonElement != null){
+    DeleteTaskButtonElement.addEventListener("click", deleteTask)
+}
+
+
+};
+//If statements
+
+//Storage
+
+//Outputs
+let allTasksOutput: HTMLDivElement = <HTMLDivElement>document.getElementById("taskListOutput");
+let allIfriendsOutput: HTMLDivElement = <HTMLDivElement>document.getElementById("statBlock")
 
 //Functions
+
+//Test Functions
+function TestButton(){
+    alert("Button works: " + sessionStorage.getItem("UserID"));
+}
 
 //User functions
 
 //Check om Username og Password matcher.
 function CheckLogin(): void{
-    axios.get<IUser[]>(webserviceUri + "User")
+    axios.get<IUser[]>(webserviceUri + "Users")
     .then(function (response: AxiosResponse<IUser[]>): void {
         let loginUsername = loginUsernameInput.value;
         let loginPassword = loginPasswordInput.value;
+        let loginUserId: number = 0;
         let loginNotFound: Boolean = true;
 
         response.data.forEach((user: IUser) => {
-            if(user.userName == loginUsername && user.password == loginPassword){
+            if(user.username == loginUsername && user.userPassword == loginPassword){
                 loginNotFound = false;
+                loginUserId =+ user.userId;
+                sessionStorage.setItem("UserID", String(loginUserId));
+                sessionStorage.setItem("UserName", user.username)
                 window.location.href = "https://tamagotchiwebapp.azurewebsites.net/createTama.htm";
 
             }
@@ -91,28 +148,137 @@ function CheckLogin(): void{
     });
 }
 
+//IFriend Functions
 
+//Skaffer FriendID ud fra IFriend navn.
+function GetFriendID(ifriendname: string): void{
+    //let TaskedIFriendName = taskFriendnameInput.value;
+    axios.get<IFriend>(webserviceUri + "Friends")
+    .then(function (response: AxiosResponse<IFriend[]>): void{
+        response.data.forEach((Ifriend: IFriend) => {
+            if(Ifriend.friendsName == ifriendname){
+                sessionStorage.setItem("FriendID", String(Ifriend.friendsId));
+            }
+            else{
+            }
+        });
+        
+    })
+    
+}
+
+//Bygger en IFriend
+function createIFriend(): void{
+    let IFriendName = IFriendNameInput.value;
+    let IFriendGender: Boolean;
+    
+    if(IFriendGenderSelector.selectedIndex == 0){
+        IFriendGender = true;
+    }
+    else if(IFriendGenderSelector.selectedIndex == 1){
+        IFriendGender = false;
+    }
+
+    axios.post<IFriend>(webserviceUri + "Friends", {ownerId: parseInt(sessionStorage.getItem("UserID")), 
+    friendsName: IFriendName, gender: IFriendGender})
+    .then((response: AxiosResponse) => { console.log("response " + response.status + " " + response.statusText); })
+        .catch((error: AxiosError) => { console.log(error); });
+
+}
+
+//Henter alle IFriends under den online bruger(Skal der kun være en?)
+function GetAllIFriends(): void{
+    let FriendFound: boolean = false;
+
+    axios.get<IFriend[]>(webserviceUri + "Friends")
+    .then(function (response: AxiosResponse<IFriend[]>): void {
+        let ifriendgenderoutput: string;
+        let result: string = "<ul>";
+        response.data.forEach((friend: IFriend) => {
+            if(friend.ownerId == parseInt(sessionStorage.getItem("UserID"))){
+            if(friend.gender == true){
+                ifriendgenderoutput = "Male";
+            }
+            else{
+                ifriendgenderoutput = "Female";
+            }
+            FriendFound = true;
+            result += 
+            "<li>" 
+            + "Friend name: " + friend.friendsName + "<br>"
+            + "Gender: " + ifriendgenderoutput + "<br>"
+            + "Hunger: " + friend.hunger + "<br>"
+            + "Thirst: " + friend.thirst + "<br>"
+            + "Fun: " + friend.fun + "<br>"
+            + "Task points: " + friend.task + "<br>"
+            + "</li>";
+            }
+            
+        });
+        if(FriendFound){
+        result += "</ul><br>";
+        allIfriendsOutput.innerHTML = result;
+        }
+        else{
+
+        }
+        
+    })
+    .catch(function (error: AxiosError): void { 
+        if (error.response) {
+            allIfriendsOutput.innerHTML = error;
+        } else {
+            allIfriendsOutput.innerHTML = error;
+        }
+    });
+}
+
+//Slet en bestemt IFriend
+function deleteIfriend(): void {  
+    GetFriendID(releaseFriendInput.value);
+    axios.delete<IFriend>(webserviceUri + "Friends/" + sessionStorage.getItem("FriendID"))
+        .then(function (response: AxiosResponse<IFriend>): void {
+            location.reload(true);
+        })
+        .catch(function (error: AxiosError): void { 
+            if (error.response) {
+               
+            } else {
+             
+            }
+        });
+}
 
 //TaskFunctions
 
 //Hent alle Tasks
-function GetAllTasks(): void{
-    axios.get<ITask[]>(webserviceUri + "Task")
+function GetIFriendTasks(): void{
+    GetFriendID(loadTaskFriendnameInput.value)
+    let FriendFound: boolean = false;
+
+    axios.get<ITask[]>(webserviceUri + "Tasks")
     .then(function (response: AxiosResponse<ITask[]>): void {
-        let result: string = "<ol>";
+        let result: string = "<ul>";
         response.data.forEach((task: ITask) => {
-            
+            if(task.friendId == parseInt(sessionStorage.getItem("FriendID"))){
+            FriendFound = true;
             result += 
             "<li>" 
-            + "Task ID: " + task.id + "<br>"
-            + "Tamagotchi ID: " + task.tamagotchiID + "<br>"
-            + "Task: " + task.taskName + "<br>"
-            + "Task Reward: " + task.taskReward
+            + "Task ID: " + task.taskId + "<br>"
+            + "Task Titel: " + task.taskName + "<br>"
+            + "Task Description: " + task.taskDesc
             +"</li>";
+            }
             
         });
-        result += "</ol>";
+        if(FriendFound){
+        result += "</ul><br>";
         allTasksOutput.innerHTML = result;
+        }
+        else{
+
+        }
+        
     })
     .catch(function (error: AxiosError): void { 
         if (error.response) {
@@ -124,18 +290,17 @@ function GetAllTasks(): void{
 }
 
 //Opret Tasks
-function PostTask(){
+function CreateTasks(){
 
 
-    //Variabler bliver sat til input values.
-    let newTaskUsername: number =+ taskUsernameInput.value;
-    let newTaskTamagotchiName: number =+ taskFriendnameInput.value
-    let newTask: string = taskNameInput.value;
-    let newTaskReward: number = taskRewards.options.selectedIndex
+    //Variabler bliver sat til input values
+    GetFriendID(taskFriendnameInput.value);
+    let newTaskTitel: string = taskTitelInput.value;
+    let newTaskDesc: string = taskDescInput.value;
     
     //Tasken bliver oprettet.
-    axios.post<ITask>(webserviceUri + "Task", 
-    {id: newTaskUsername, tamagotchiID: newTaskTamagotchiName, taskName: newTask, taskReward: newTaskReward})
+    axios.post<ITask>(webserviceUri + "Tasks", 
+    {friendId: parseInt(sessionStorage.getItem("FriendID")), taskName: newTaskTitel, taskDesc: newTaskDesc})
     .then((response: AxiosResponse) => { console.log("response " + response.status + " " + 
     response.statusText); })
     .catch((error: AxiosError) => { console.log(error); });
@@ -143,7 +308,19 @@ function PostTask(){
 }
 
 //Slet Tasks
-
+function deleteTask(): void {  
+    axios.delete<ITask>(webserviceUri + "Tasks/" + deleteTaskIdInput.value)
+        .then(function (response: AxiosResponse<ITask>): void {
+            location.reload(true);
+        })
+        .catch(function (error: AxiosError): void { 
+            if (error.response) {
+               
+            } else {
+             
+            }
+        });
+}
 
 
 //Slet alle Tasks
